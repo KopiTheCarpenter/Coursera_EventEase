d16eb9d3895e0c24f022c11022ca30dc8985f29d
KopiTheCarpenter <62626898+KopiTheCarpenter@users.noreply.github.com>
2025-11-22 18:33:36 +0100

    Activity2

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventEase.Models
 {
     public class Event
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Event name is required")]
+        [StringLength(100, ErrorMessage = "Event name must be under 100 characters")]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; } = string.Empty;
     }
 }
